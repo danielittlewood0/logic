bcffd12534c8d6b1be4911c5f79aeadd25440685
danielittlewood0 <danielittlewood@gmail.com>
2018-12-17 21:47:48 +0000

    deduction theorem works but can't see steps

--- propositional_logic/propositions.rb
+++ propositional_logic/propositions.rb
@@ -90,9 +90,10 @@ class Proof
   end
 
   def expand_hypotheses
-    hyps = hypotheses
+    hyps = hypotheses.dup
     hyps.each do |prop|
       if (prop.class == PropositionImplies) && hyps.include?(prop.pre) && !hyps.include?(prop.post)
+        puts "I deduced #{prop.post.print}"unless hyps.include?(prop.post)
         hyps << prop.post unless hyps.include?(prop.post)
       end
       if (prop.is_a?(PropositionNot)) && (prop.args[0].is_a?(PropositionNot))
@@ -104,13 +105,17 @@ class Proof
     proof(*hyps)
   end
 
-  def try_assumptions(conc,steps)
+  def try_assumptions!(conc,steps)
     old_hyps = hyps
     old_hyps.each do |prop| 
-      if prop.is_a?(PropositionImplies) && !old_hyps.include?(prop.pre)
-        if proof(*(old_hyps + [prop.pre])).entails(conc)
+      if conc.is_a?(PropositionImplies) && !old_hyps.include?(conc.pre)&& !old_hyps.include?(conc)
+        puts "I tried assuming #{conc.pre.print}"
+        puts ">>"
+        if proof(*(old_hyps + [conc.pre])).entails(conc.post)
+          puts "<<"
+          puts "So I deduced #{conc.print}"
           self.hypotheses << conc
-          steps << proof(*(old_hyps + [prop.pre])).entails(conc)
+          steps << proof(*(old_hyps + [conc.pre])).entails(conc)
         end
       end
     end
@@ -123,19 +128,23 @@ class Proof
   end
 
   def entails(conclusion,steps=[])
-    p "*****"
     if obviously_entails(conclusion)
       return true
-      return steps
     else
       old_hyps = hypotheses
       expanded = expand_hypotheses
-      p hyps.map(&:print)
-      try_assumptions(conclusion,steps)
-      p hyps.map(&:print)
-      return nil if hyps == old_hyps
+      if expanded.obviously_entails(conclusion)
+        return true
+      end
+      expanded.try_assumptions!(conclusion,steps)
+      if expanded.obviously_entails(conclusion)
+        return true
+      end
+      if expanded == proof(*old_hyps)
+        return nil
+      end
       steps << expanded 
-      entails(conclusion,steps)
+      return expanded.entails(conclusion,steps)
     end
   end
 
@@ -148,11 +157,23 @@ def and(*args)
   PropositionAnd.new(args)
 end
 
+def land(*args)
+  PropositionAnd.new(args)
+end
+
 def or(*args)
   PropositionOr.new(args)
 end
 
+def lor(*args)
+  PropositionOr.new(args)
+end
+
 def not(*args)
+  PropositionNot.new(args)
+end
+
+def neg(*args)
   PropositionNot.new(args)
 end
 
--- propositional_logic/propositions_spec.rb
+++ propositional_logic/propositions_spec.rb
@@ -1,19 +1,21 @@
 require './propositions.rb'
 
 describe Proposition do
-  it "test" do 
-    q = PropositionAtomic.new('q')
-    r = PropositionAtomic.new('r')
-    s = PropositionAnd.new([q,r])
+  let(:q){atom('q')}
+  let(:r){atom('r')}
+  let(:a){atom('a')}
+  let(:b){atom('b')}
+  let(:c){atom('c')}
+
+  it "pretty print" do 
+    s = land(q,r)
     expect(q.print).to eq 'q'
     expect(q.name).to eq 'q'
     expect(s.print).to eq '(q \\wedge r)'
   end
 
   describe '#substitute' do
-    it 'replaces things by same string' do 
-      q = PropositionAtomic.new('q')
-      r = PropositionAtomic.new('r')
+    it 'replaces props == by .to_s' do 
       s = PropositionAnd.new([q,r])
       new_s = s.substitute(q,r)
       expect(new_s.print).to eq "(r \\wedge r)"
@@ -21,30 +23,42 @@ describe Proposition do
     end
   end
 
+  describe '#expand_hypotheses' do
+    it 'inserts b if the proof contains a and a \\rightarrow b' do
+      old_proof = proof(a,a.implies(b))
+      new_proof = old_proof.expand_hypotheses
+      expect(new_proof).to eq proof(a,a.implies(b),b)
+    end
+
+    it 'inserts a if the proof contains not(not(a))' do
+      old_proof = proof(neg(neg(a)))
+      new_proof = old_proof.expand_hypotheses
+      expect(new_proof).to eq proof(neg(neg(a)),a)
+    end
+  end
+
   describe '#obviously_entails, #entails' do
     it 'checks if hypotheses entail by modus ponens' do 
-      a = atom('a')
-      b = atom('b')
       a_b = a.implies(b)
       expect(proof(a,a_b).obviously_entails(b)).to eq nil
       expect(proof(a,a_b).expand_hypotheses.obviously_entails(b)).to eq true
     end
 
     it 'chains applications of modus ponens' do 
-      a = atom('a')
-      b = atom('b')
       a_b = a.implies(b)
       expect(proof(a,a_b).entails(b)).to eq true
     end
 
-    it 'checks if hypotheses entail by modus ponens' do 
-      a = atom('a')
-      b = atom('b')
-      c = atom('c')
+    it 'deduction theorem' do 
       a_b = a.implies(b)
       b_c = b.implies(c)
       a_c = a.implies(c)
       expect(proof(a_b,b_c).entails(a_c)).to eq true
+    end
+
+    it 'deduction theorem 2' do 
+      a_b = a.implies(b)
+      expect(proof(b).entails(a_b)).to eq true
     end
   end
 
